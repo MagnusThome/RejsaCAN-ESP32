1fe97b204800754fe72c267356094e4003fa0193
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2021-10-15 18:54:37 +0200

    Update main.ino

--- examples/main.ino
+++ examples/main.ino
@@ -1,16 +1,23 @@
-#define LED_BLUE           4  // GPIO4
-#define LED_YELLOW        12  // GPIO12
+#define BLUE_LED           4  // GPIO4
+#define YELLOW_LED        12  // GPIO12
 #define FORCE_KEEP_ON     25  // GPIO25 
 #define POWER_IN_VOLTAGE  26  // GPIO26
 
-#define ENGINE_STOPPED_VOLTAGE_THRESHOLD  900
-#define SECONDS_TO_FORCE_ON               180
+#define ENGINE_STOPPED_VOLT_THRESHOLD  900
+#define SECONDS_TO_STAY_ON             180
 
+
+void shutdowncounter(unsigned long secondsToStayOn);
+
+
+
+
+// -----------------------------------------------------------------------------------
 
 void setup() {
 
-  pinMode(LED_BLUE, OUTPUT);
-  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  pinMode(YELLOW_LED, OUTPUT);
   
   pinMode(FORCE_KEEP_ON, OUTPUT);
   digitalWrite(FORCE_KEEP_ON, HIGH);
@@ -19,24 +26,36 @@ void setup() {
 
 
 
+// -----------------------------------------------------------------------------------
 
 void loop() {
 
-  unsigned long now = millis();
-  unsigned long countdown = 0;
-
-
-  if( analogRead( POWER_IN_VOLTAGE) > ENGINE_STOPPED_VOLTAGE_THRESHOLD ) {
-    countdown = 0;
-  }
-  else {
-    if ( countdown == 0 ) {
-      countdown = millis();                             // TIMESTAMP OF WHEN ENGINE STOPPED
-    }
-    if ( countdown >= SECONDS_TO_FORCE_ON * 1000 ) {    // TIME TO SHUT BOARD OFF
-      digitalWrite(FORCE_KEEP_ON, LOW);   
-    }
-  }
- 
+  shutdowncounter(SECONDS_TO_STAY_ON);
 
 }
+
+
+
+// -----------------------------------------------------------------------------------
+
+void shutdowncounter(unsigned long secondsToStayOn) {
+
+  static bool countdown = false;
+  static unsigned long timerstart;
+
+  if( analogRead(POWER_IN_VOLTAGE) < ENGINE_STOPPED_VOLT_THRESHOLD ) {
+    if ( countdown == false ) {
+      timerstart = millis();                                 // START COUNTDOWN
+    }
+    countdown = true;
+    if ( millis() - timerstart >= secondsToStayOn * 1000 ) {
+      digitalWrite(FORCE_KEEP_ON, LOW);                      // TIME TO SHUT DOWN
+    }
+  }
+  else {
+    countdown = false;
+  }
+}
+
+
+// -----------------------------------------------------------------------------------

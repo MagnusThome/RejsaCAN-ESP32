f072f1e973ed760f227b4231eed05c54ef75fd33
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2022-10-09 15:23:31 +0200

    Create pinout.h

---
+++ pinout.h
@@ -0,0 +1,54 @@
+// Pinout v3.2
+
+#define FORCE_ON    17
+#define SENSE_V_DIG 8
+#define SENSE_V_ANA 9
+
+#define BLUE_LED    10
+#define YELLOW_LED  11
+
+#define CAN_RX      13
+#define CAN_TX      14
+#define CAN_RS      38    // Standby and/or faster rise times (from v3.2 only)
+
+#define HI_DRIVER   21    // 3V3 high driver power output
+
+#define SDA         1     // I2C 
+#define SCL         2     // I2C
+
+#define MISO        41    // SPI
+#define MOSI        40    // SPI
+#define CLK         39    // SPI
+#define SD_CARD     45    // SPI CS for SD card reader
+
+/*
+
+Total list of available solderable breakout pads are:
+
+GPIO01  or I2C SDA
+GPIO02  or I2C SCL
+GPIO39  or SPI CLK
+GPIO40  or SPI MOSI
+GPIO41  or SPI MISO
+GPIO42  or JTAG MTMS
+
+GPIO12  no default use
+GPIO48  no default use
+GPIO47  no default use
+GPIO21  Power output driver, not actual GPIO pin
+
+RXD     second UART
+TXD     second UART
+
+GPIO10  LED only, no pad!
+GPIO11  LED only, no pad!
+
+GPIO06  extra
+GPIO07  extra
+GPIO15  extra
+GPIO16  extra
+
+GPIO0   PROGR mode strapping pin
+GPIO03  JTAG_ENABLE strapping pin
+
+*/

1a759024fb52128ec6397fc631d9e79f8ee73b41
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2023-02-09 17:53:45 +0100

    Create s3-sdcard.ino

---
+++ examples/ESP32-S3 - SPI settings for SD card reader/s3-sdcard.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include <SD.h>
+
+#define MISO  41
+#define MOSI  40
+#define SCK   39
+#define CS    45
+
+File root;
+
+
+void setup() {
+  Serial.begin(115200);
+  delay(200);
+  SPI.begin( SCK, MISO, MOSI, CS );
+}
+
+
+
+void loop() {
+  if (SD.begin(CS)) {
+    root = SD.open("/");
+    printDirectory(root, 0);
+  }
+  else {
+    Serial.println("no card... ");
+  }
+  delay(5000);
+}
+
+
+
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}

016a63512e72a810558b7e5397b61a56def91ee1
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2023-04-04 10:23:37 +0200

    message.extd fixed

--- examples/ESP32-S3 - simple first test in car/firsttestincar_s3.ino
+++ examples/ESP32-S3 - simple first test in car/firsttestincar_s3.ino
@@ -29,11 +29,12 @@ uint16_t rpm;
 
 void setup() {
   Serial.begin(115200);
+  Serial.setTxTimeoutMs(0); // prevent slow serial prints if no usb
   pinMode(YELLOW_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
   pinMode(CAN_RS, INPUT);     // INPUT (high impedance) = slope control mode, OUTPUT = see next line
   digitalWrite(CAN_RS, LOW);  // LOW = high speed mode, HIGH = low power mode (listen only)
-  twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)TX_PIN, (gpio_num_t)RX_PIN, TWAI_MODE_NO_ACK);  // TWAI_MODE_NORMAL, TWAI_MODE_NO_ACK or TWAI_MODE_LISTEN_ONLY
+  twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)TX_PIN, (gpio_num_t)RX_PIN, TWAI_MODE_NORMAL);  // TWAI_MODE_NORMAL, TWAI_MODE_NO_ACK or TWAI_MODE_LISTEN_ONLY
   twai_timing_config_t t_config  = TWAI_TIMING_CONFIG_500KBITS();
   twai_filter_config_t f_config  = TWAI_FILTER_CONFIG_ACCEPT_ALL();
   twai_driver_install(&g_config, &t_config, &f_config);
@@ -57,7 +58,7 @@ int CANsend (void) {
   
   twai_message_t message;
   message.identifier = CAN_REQST_ID;
-  message.extd = 1;
+  message.extd = 0;
   message.data_length_code = 8;
   message.data[0] = 2;
   message.data[1] = 1;

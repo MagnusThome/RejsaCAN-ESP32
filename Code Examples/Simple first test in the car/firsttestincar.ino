f28ed19f8a19a3deb25c0921207cd4b458c460a8
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2023-12-23 20:27:02 +0100

    all in one S3 and non S3

--- Code Examples/Simple first test in the car/firsttestincar.ino
+++ Code Examples/Simple first test in the car/firsttestincar.ino
@@ -4,37 +4,62 @@ Good for an initial test in the car to check that the board and the car's OBD co
 
 - Blue LED blinks when CAN replies are received from the car
 - Yellow LED is on when the engine rpm is over 2000 rpm
-- Engine rpms are continously printed over the Bluetooth connection
+- On RejsaCAN v2.x - NON S3 ESP32 - the engine rpms are continously printed over the Bluetooth connection
 
+--------------------
+
+Comment out everything about mcp2517fd and mcp2515 in the /esp32_can/src/ esp32_can.cpp and esp32_can.h library files
+
+--------------------
 */
 
-#include "esp32_can.h"            // https://github.com/collin80/esp32_can AND https://github.com/collin80/can_common
-#include "BluetoothSerial.h"
+#define ESP32S3   // COMMENT THIS OUT FOR RejsaCAN 2.x NON S3 ESP32 BOARDS
 
-#define BLUE_LED          13  // GPIO13  (GPIO4 on v2.0)
-#define YELLOW_LED        12  // GPIO12
+
+#include "esp32_can.h"            // https://github.com/collin80/esp32_can AND https://github.com/collin80/can_common
 
 #define CANPID_RPM          0x0C
 #define CAN_REQST_ID        0x7DF 
 #define CAN_REPLY_ID        0x7E8
 
+#ifdef ESP32S3
+  #define BLUE_LED      10
+  #define YELLOW_LED    11
+  #define RX_PIN        13
+  #define TX_PIN        14
+  #define CAN_RS        38
+#else
+  #include "BluetoothSerial.h"
+  #define BLUE_LED      13
+  #define YELLOW_LED    12
+  #define RX_PIN        16
+  #define TX_PIN        17
+  BluetoothSerial SerialBT;
+#endif
+
+
 uint16_t rpm;
-BluetoothSerial SerialBT;
 
 
 void setup() {
   Serial.begin(115200);
-  SerialBT.begin("RejsaCAN");
   Serial.println("Boot");
-  SerialBT.println("Boot");
 
-  pinMode(27, OUTPUT); // THE CAN LIBRARY HAS THIS PIN FOR INTERRUPT FOR CAN1 (UNSUSED HERE) INPUT WITHOUT PULLUP, FORCE TO OUTPUT INSTEAD TO PREVENT ERRONEOUS INTERRUPTS.
-  
+#ifdef ESP32S3
+  Serial.setTxTimeoutMs(0);   // prevent serial print delays if no usb
+  pinMode(CAN_RS, OUTPUT);    // INPUT (high impedance) = slope control mode, OUTPUT = see next line
+  digitalWrite(CAN_RS, LOW);  // LOW = high speed mode, HIGH = low power mode (listen only)
+#else
+  SerialBT.begin("RejsaCAN");
+  SerialBT.println("Boot");
+#endif
+
   pinMode(YELLOW_LED, OUTPUT);
   pinMode(BLUE_LED, OUTPUT);
   digitalWrite(YELLOW_LED, LOW);
   digitalWrite(BLUE_LED, LOW);
 
+  CAN0.setCANPins((gpio_num_t)RX_PIN, (gpio_num_t)TX_PIN);
   CAN0.begin(CAN_BPS_500K);
   CAN0.watchFor(CAN_REPLY_ID);
   CAN0.setCallback(0, callback);
@@ -45,7 +70,9 @@ void loop() {
   if( rpm > 2000 ) { digitalWrite(YELLOW_LED, HIGH); }
   else             { digitalWrite(YELLOW_LED, LOW);  }
   Serial.println(rpm);
+#ifndef ESP32S3
   SerialBT.println(rpm);
+#endif
   delay(200);
 }
 
